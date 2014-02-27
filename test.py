fd2b67b7e7aeabbeb9c14722866337ff968a0564
Tobias Reese <tobias.reese@gmail.com>
2014-02-27 22:34:07 +0100

    tried to fix 2.6 compatibility

--- test.py
+++ test.py
@@ -114,8 +114,9 @@ class FFmpegTestCase(unittest.TestCase):
         self.assertEqual(list(ffmpeg), ['ffmpeg', '-i', '/old', '-threads:v', '0', '/new'])
 
     def test_stream_type_error(self):
-        with self.assertRaisesRegexp(ValueError, 'z is an unsupported stream_type.'):
+        with self.assertRaises(ValueError) as context:
             Stream(stream_type='z')
+        self.assertEqual(context.exception.message, 'z is an unsupported stream_type.')
 
     def test_metadata(self):
         stream = Stream()
